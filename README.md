cb4e9bd9b2f405e8c88c60c1dbdd40997c37cc7b
Jonah McCaffrey <jonahmac234@google.com>
2025-09-10 23:44:33 +0100

    Added initial README, added flake output modules

---
+++ README.md
@@ -0,0 +1,1 @@
+# A Nix project for installing multiple Neovim configurations under different package names.
--- flake.nix
+++ flake.nix
@@ -5,14 +5,20 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    packages.x86_64-linux.neovim-custom = import ./neovim-custom.nix {inherit pkgs;};
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.neovim-custom;
+  outputs = inputs:
+  # let
+  #   systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
+  #   eachSystem = nixpkgs.lib.genAttrs systems;
+  # in
+  #   eachSystem (system: let
+  #     pkgs = import nixpkgs {
+  #       inherit system;
+  #     };
+  #   in {
+  #
+  #   });
+  {
+    nixosModules.default = import ./modules/nixosModule;
+    homeModules.default = import ./modules/homeModule;
   };
 }
