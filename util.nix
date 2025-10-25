754a5052c35353fbe27be6998bf755435f621c21
Jonah McCaffrey <jonahmac234@googlemail.com>
2025-10-25 22:51:32 +0100

    Testing pkgs.buildEnv instead of pkgs.symlinkJoin

--- util.nix
+++ util.nix
@@ -21,12 +21,32 @@ in {
         then pkgConf.dependencies
         else [];
     in
-      pkgs.symlinkJoin {
+      # pkgs.symlinkJoin {
+      #   name = pkgName; # Custom package name
+      #
+      #   paths = [nvimPackage] ++ runtimeDeps;
+      #
+      #   nativeBuildInputs = [pkgs.makeWrapper];
+      #
+      #   postBuild = ''
+      #     mv $out/bin/nvim $out/bin/${pkgName}  # Custom binary name
+      #     wrapProgram $out/bin/${pkgName} \
+      #       --set NVIM_APPNAME ${configName}  # Custom config dir: ~/.config/nvim-custom
+      #   '';
+      # }
+      pkgs.buildEnv {
         name = pkgName; # Custom package name
 
         paths = [nvimPackage] ++ runtimeDeps;
 
         nativeBuildInputs = [pkgs.makeWrapper];
+
+        pathsToLink = [
+          "/lib" # Libraries for runtime linking
+          "/lib64" # 64-bit libs (if applicable)
+          "/include" # Headers, if needed
+          "/share" # Data/config files
+        ];
 
         postBuild = ''
           mv $out/bin/nvim $out/bin/${pkgName}  # Custom binary name
