42e246e4673bf382c4d14b048a56591830ffe928
Jonah McCaffrey <jonahmac234@googlemail.com>
2025-10-25 21:52:54 +0100

    Renamed "packages" to "configurations" and refactored wrapper function

--- modules/homeModule/default.nix
+++ modules/homeModule/default.nix
@@ -5,14 +5,14 @@
   ...
 }: let
   inherit (builtins) hasAttr;
-  inherit (lib) mkOption mkEnableOption mkIf mapAttrsToList flatten;
+  inherit (lib) mkOption mkEnableOption mkIf mapAttrsToList flatten unique;
   inherit (lib.types) attrs;
   cfg = config.programs.multi-nvim;
 in {
   options = {
     programs.multi-nvim = {
       enable = mkEnableOption "multi-nvim";
-      packages = mkOption {
+      configurations = mkOption {
         type = attrs;
         default = {};
         description = "Attribute set of wrapped neovim packages.";
@@ -21,40 +21,43 @@ in {
   };
   config = mkIf cfg.enable (
     let
-      dependencies = flatten (mapAttrsToList (_: value:
+      # Extract package dependencies for all configurationsh
+      dependencies = unique (flatten (mapAttrsToList (_: value:
         if hasAttr "dependencies" value
         then value.dependencies
         else [])
-      cfg.packages);
+      cfg.configuration));
+
+      # Function to generate the wrapped Neovim package
+      pkgGenFunction = mapAttrsToList (
+        pkgName: pkgConf: let
+          pkgs' =
+            if hasAttr "pkgs" pkgConf
+            then pkgConf.pkgs
+            else pkgs;
+          configName =
+            if hasAttr "configName" pkgConf
+            then pkgConf.configName
+            else pkgName;
+        in
+          pkgs.symlinkJoin {
+            name = pkgName; # Custom package name
+
+            paths = [pkgs'.neovim];
+
+            nativeBuildInputs = [pkgs.makeWrapper];
+
+            postBuild = ''
+              mv $out/bin/nvim $out/bin/${pkgName}  # Custom binary name
+              wrapProgram $out/bin/${pkgName} \
+                --set NVIM_APPNAME ${configName}  # Custom config dir: ~/.config/nvim-custom
+            '';
+          }
+      );
     in {
       home.packages =
         dependencies
-        ++ mapAttrsToList (
-          pkgName: pkgConf: let
-            pkgs' =
-              if hasAttr "pkgs" pkgConf
-              then pkgConf.pkgs
-              else pkgs;
-            configName =
-              if hasAttr "configName" pkgConf
-              then pkgConf.configName
-              else pkgName;
-          in
-            pkgs.symlinkJoin {
-              name = pkgName; # Custom package name
-
-              paths = [pkgs'.neovim];
-
-              nativeBuildInputs = [pkgs.makeWrapper];
-
-              postBuild = ''
-                mv $out/bin/nvim $out/bin/${pkgName}  # Custom binary name
-                wrapProgram $out/bin/${pkgName} \
-                  --set NVIM_APPNAME ${configName}  # Custom config dir: ~/.config/nvim-custom
-              '';
-            }
-        )
-        cfg.packages;
+        ++ (pkgGenFunction cfg.configuration);
     }
   );
 }
